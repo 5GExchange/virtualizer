b09d80be061fdef4d03b640838c49cf3d192a911
Allan Vidal <allan.vidal@ericsson.com>
2016-05-18 14:59:17 -0300

    Add virtualizer_color script: stdin -> Virtualizer pretty output

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='virtualizer',
       version='5.0.0',
       description='UNIFY virtualizer',
       author='Ericsson',
-      py_modules = ['virtualizer', 'baseclasses', 'virtualizer_utils']
+      py_modules=['virtualizer', 'baseclasses', 'virtualizer_utils'],
+      scripts=['virtualizer_color']
 )
