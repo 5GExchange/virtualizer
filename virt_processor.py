a85d45e059a324a04de4229e954f7ea0a841bb78
Akos <recseakos@gmail.com>
2016-07-15 12:42:28 +0200

    SAP connection fixed in virt_processor
    Signed-off-by: Akos <recseakos@gmail.com>

--- virt_processor.py
+++ virt_processor.py
@@ -8,14 +8,13 @@ def combine(self, source):
     old_self = copy.deepcopy(self)
     self.merge(source)
     for virt1_node in old_self.nodes.node:
+        virt1_saps = {}
+        for port in old_self.nodes[virt1_node].ports.port:
+            if old_self.nodes[virt1_node].ports[port].port_type.get_as_text() == 'port-sap':
+                virt1_saps[old_self.nodes[virt1_node].ports[port].sap.get_as_text()]=old_self.nodes[virt1_node].ports[port].id.get_as_text()
+
         for virt2_node in source.nodes.node:
-            virt1_saps = {}
             virt2_saps = {}
-
-            for port in old_self.nodes[virt1_node].ports.port:
-                if old_self.nodes[virt1_node].ports[port].port_type.get_as_text() == 'port-sap':
-                    virt1_saps[old_self.nodes[virt1_node].ports[port].sap.get_as_text()]=old_self.nodes[virt1_node].ports[port].id.get_as_text()
-
 
             for port in source.nodes[virt2_node].ports.port:
                 if source.nodes[virt2_node].ports[port].port_type.get_as_text() == 'port-sap':
@@ -35,12 +34,12 @@ def combine(self, source):
                 link.src = link.src.get_rel_path(self.nodes[virt1_node].ports[virt1_port])
                 link.dst = link.dst.get_rel_path(self.nodes[virt2_node].ports[pairs[virt1_port]])
 
-                link = virtualizer.Link(id='automaicliadded_'+pairs[virt1_port]+'_'+virt1_port,
-                                        name='aa_link',
-                                        resources=virtualizer.Link_resource(delay=0, bandwidth=0)
-                                        )
-
-
-                self.links.add(link)
-                link.src = link.dst.get_rel_path(self.nodes[virt2_node].ports[pairs[virt1_port]])
-                link.dst = link.src.get_rel_path(self.nodes[virt1_node].ports[virt1_port])
+                # link = virtualizer.Link(id='automaicliadded_'+pairs[virt1_port]+'_'+virt1_port,
+                #                         name='aa_link',
+                #                         resources=virtualizer.Link_resource(delay=0, bandwidth=0)
+                #                         )
+                #
+                #
+                # self.links.add(link)
+                # link.src = link.dst.get_rel_path(self.nodes[virt2_node].ports[pairs[virt1_port]])
+                # link.dst = link.src.get_rel_path(self.nodes[virt1_node].ports[virt1_port])
