0905f107c9e11b9639be271b4dcf63bf71f8e800
Robert Szabo <robert.szabo@ericsson.com>
2017-03-04 20:57:25 +0100

    setup update and script added back

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='virtualizer',
       version='5.0.0',
       description='UNIFY virtualizer',
       author='Ericsson',
-      py_modules=['virtualizer', 'baseclasses', 'virtualizer_utils'],
+      py_modules=['virtualizer', 'baseclasses', 'virtualizer_info', 'virtualizer_mappings'],
       scripts=['virtualizer_color']
 )
